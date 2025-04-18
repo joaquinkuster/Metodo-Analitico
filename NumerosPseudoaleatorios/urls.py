5c0495c4fdf9b6a96e7ba5488bcdf34e64bbf05a
joapurete <joaquinkuster3000@gmail.com>
2025-04-18 02:07:09 -0300

    se hicieron modificaciones en el modelo, se dividieron en dos tablas las secuencias generadas (congruencial y von neumann), se avanzo con la interfaz grafica y se adapto la logica de generacion de numeros aleatorios para que guarde los numeros en la bd

--- NumerosPseudoaleatorios/urls.py
+++ NumerosPseudoaleatorios/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('generar', views.generar),
+    path('', views.index, name='index'),
+    path('generar', views.generar, name='generar'),
+    path('testear', views.generar, name='testear')
 ]
